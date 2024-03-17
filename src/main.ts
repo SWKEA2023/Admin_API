7f76a2da036731740c395f65f8f27688cce08ef5
Roed <Geofery10@hotmail.com>
2024-03-18 00:12:08 +0100

    setup swagger with microservice in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,37 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { INestApplication } from '@nestjs/common';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
+
+function setupSwagger(app: INestApplication): void {
+  const documentBuilder = new DocumentBuilder()
+    .setTitle('Nest.js example')
+    .setDescription('This is example for nest.js')
+    .setVersion('1.0')
+    .addBasicAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, documentBuilder);
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: { defaultModelsExpandDepth: -1 },
+  });
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const config = new DocumentBuilder()
-    .setTitle('Project API')
-    .setDescription('Project API description')
-    .setVersion('1.0')
-    .addTag('Project')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://localhost:5672'],
+      queue: 'main_queue',
+      queueOptions: {
+        durable: false,
+      },
+    },
+  });
+
+  setupSwagger(app);
   await app.listen(3000);
 }
 
