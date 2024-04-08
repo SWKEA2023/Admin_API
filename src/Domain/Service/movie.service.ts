35fecc500366f0d2efdbc400811ebadb30e78c88
Roed <Geofery10@hotmail.com>
2024-04-08 14:35:29 +0200

    finished service layer Movie

---
+++ src/Domain/Service/movie.service.ts
@@ -0,0 +1,26 @@
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
+import { CreateMovieCommand } from '../../Application/Movie/Commands/Impl/create-movie.command';
+import { Movie } from '../../Domain/Entities/Movie';
+import { GetMovieQuery } from '../../Application/Movie/Queries/Impl/get-movie.query';
+import { GetMoviesQuery } from '../../Application/Movie/Queries/Impl/get-movies.query';
+
+export class MovieService {
+  constructor(
+    private readonly commandBus: CommandBus,
+    private readonly queryBus: QueryBus,
+  ) {}
+
+  async createMovie(movie: Movie) {
+    return this.commandBus.execute(
+      new CreateMovieCommand(movie.movieId, movie.title, movie.duration),
+    );
+  }
+
+  async getMovie(id: number) {
+    return this.queryBus.execute(new GetMovieQuery(id));
+  }
+
+  async getMovies() {
+    return this.queryBus.execute(new GetMoviesQuery());
+  }
+}
--- src/Interface/Controllers/movie.controller.ts
+++ src/Interface/Controllers/movie.controller.ts
@@ -1,31 +1,23 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
-import { CommandBus, QueryBus } from '@nestjs/cqrs';
-import { CreateMovieCommand } from '../../Application/Movie/Commands/Impl/create-movie.command';
+import { MovieService } from '../../Domain/Service/movie.service';
 import { Movie } from '../../Domain/Entities/Movie';
-import { GetMovieQuery } from '../../Application/Movie/Queries/Impl/get-movie.query';
-import { GetMoviesQuery } from '../../Application/Movie/Queries/Impl/get-movies.query';
 
 @Controller('movie')
 export class MovieController {
-  constructor(
-    private readonly commandBus: CommandBus,
-    private readonly queryBus: QueryBus,
-  ) {}
+  constructor(private readonly movieService: MovieService) {}
 
   @Post()
   async createMovie(@Body() movie: Movie) {
-    return this.commandBus.execute(
-      new CreateMovieCommand(movie.movieId, movie.title, movie.duration),
-    );
+    return this.movieService.createMovie(movie);
   }
 
   @Get(':id')
   async getMovie(@Param('id') id: number) {
-    return this.queryBus.execute(new GetMovieQuery(id));
+    return this.movieService.getMovie(id);
   }
 
   @Get()
   async getMovies() {
-    return this.queryBus.execute(new GetMoviesQuery());
+    return this.movieService.getMovies();
   }
 }
