0f4d66afcf25040d346174f32a39079fd3573d0c
Joachim Tyrfing Richter <joac0724@stud.kea.dk>
2024-04-09 23:01:55 +0200

    added: boilerplate for swagger setup

--- src/Domain/Entities/Movie.ts
+++ src/Domain/Entities/Movie.ts
@@ -1,14 +1,28 @@
 import { AggregateRoot } from '@nestjs/cqrs';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Movie extends AggregateRoot {
+  @ApiPropertyOptional({
+    type: Number,
+    description: 'This is an optional property',
+    readOnly: true,
+  })
   @PrimaryGeneratedColumn()
   movieId: number;
 
+  @ApiProperty({
+    type: String,
+    description: 'This is a required property',
+  })
   @Column()
   title: string;
 
+  @ApiProperty({
+    type: Number,
+    description: 'This is a required property',
+  })
   @Column()
   duration: number;
 
